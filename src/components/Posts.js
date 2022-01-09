2aad8d4634fb4bfb2e0b8b8e236c57fdf3ec2828
Stephen Carroll <stephenalexandercarroll@gmail.com>
2022-01-09 10:53:22 -0500

    Minor refactor of Posts
    Still needs work but is own component for now.
    Can we make this more reusable in the future?

---
+++ src/components/Posts.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import Card from './Card'
+import PropTypes from 'prop-types'
+import { FaUser, FaCompass, FaBriefcase, FaBook, FaUserFriends, FaUserPlus, FaTwitter, FaStar } from 'react-icons/fa'
+
+function Posts ({ posts }) {
+    const [currentPage, setCurrentPage] = React.useState(1)
+    const [postsPerPage, setPostsPerPage] = React.useState(10)
+    
+    console.log(posts)
+    return (
+        <ul className='grid space-around'>
+            {posts.map((post) => {
+                if (post.node.login) {
+                    // Is this a little wonky? Do you think there's a way to do this more cleanly?
+                    const { login, avatarUrl, url, name, company, location, bio, twitterUsername } = post.node
+                    const followers = post.node.followers.totalCount
+                    const following = post.node.following.totalCount
+                    const starredRepos = post.node.starredRepositories.totalCount
+
+                    return (
+                        <li key={post.id}>
+                            <Card
+                                header={login}
+                                avatar={avatarUrl}
+                                href={url}
+                            />
+                            <ul className='card-list'>
+                                <li>
+                                    <a href={url}>
+                                        {login}
+                                    </a>
+                                </li>
+                                {name &&
+                                    <li>
+                                        <FaUser color='rgb(239, 115, 115)' size={22} />
+                                        {name}
+                                    </li>
+                                }
+                                {company &&
+                                    <li>
+                                        <FaBriefcase color='#795548' size={22} />
+                                        {company}
+                                    </li>
+                                }
+                                {location &&
+                                    <li>
+                                        <FaCompass color='rgb(144, 115, 255)' size={22} />
+                                        {location}
+                                    </li>
+                                }
+                                {bio &&
+                                    <li>
+                                        <FaBook color='rgb(172, 209, 175)' size={22} />
+                                        {bio}
+                                    </li>
+                                }
+                                <li>
+                                    <FaUserFriends color='#E0D1A6' size={22}/>{followers}
+                                    <FaUserPlus color='#7AD7F0' size={22}/>{following}
+                                </li>
+                                {twitterUsername &&
+                                    <li>
+                                        <FaTwitter color='rgb(0, 191, 255)' size={22} />
+                                        {twitterUsername}
+                                    </li>
+                                }
+                                <li>
+                                    <FaStar color='#AA8F00' size={22} />
+                                    {starredRepos}
+                                </li>
+                            </ul>
+                        </li>
+                    )
+                }
+            })}
+        </ul>
+    )
+}
+
+Posts.propTypes = {
+    users: PropTypes.array.isRequired
+}
+
+export default Posts
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,80 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Posts from './Posts'
 import { fetchMultipleUsersGQL } from '../utils/api'
-import Card from './Card'
-import { FaUser, FaCompass, FaBriefcase, FaBook, FaUserFriends, FaUserPlus, FaTwitter } from 'react-icons/fa'
-
-function DisplayUsers({ users }) {
-    console.log(users)
-    return (
-        <ul className='grid space-around'>
-            {users.map((user) => {
-                if (user.node.login) {
-                    // Is this a little wonky? Do you think there's a way to do this more cleanly?
-                    const { login, avatarUrl, url, name, company, location, bio, twitterUsername } = user.node
-                    const followers = user.node.followers.totalCount
-                    const following = user.node.following.totalCount
-                    const starredRepos = user.node.starredRepositories.totalCount
-
-                    return (
-                        <li key={user.id}>
-                            <Card
-                                header={login}
-                                avatar={avatarUrl}
-                                href={url}
-                            />
-                            <ul className='card-list'>
-                                <li>
-                                    <a href={url}>
-                                        {login}
-                                    </a>
-                                </li>
-                                {name &&
-                                    <li>
-                                        <FaUser color='rgb(239, 115, 115)' size={22} />
-                                        {name}
-                                    </li>
-                                }
-                                {company &&
-                                    <li>
-                                        <FaBriefcase color='#795548' size={22} />
-                                        {company}
-                                    </li>
-                                }
-                                {location &&
-                                    <li>
-                                        <FaCompass color='rgb(144, 115, 255)' size={22} />
-                                        {location}
-                                    </li>
-                                }
-                                {bio &&
-                                    <li>
-                                        <FaBook color='rgb(172, 209, 175)' size={22} />
-                                        {bio}
-                                    </li>
-                                }
-                                <li>
-                                    <FaUserFriends color='#E0D1A6' size={22}/>{followers}
-                                    <FaUserPlus color='#7AD7F0' size={22}/>{following}
-                                </li>
-                                {twitterUsername &&
-                                    <li>
-                                        <FaTwitter color='rgb(0, 191, 255)' size={22} />
-                                        {twitterUsername}
-                                    </li>
-                                }
-                            </ul>
-                        </li>
-                    )
-                }
-            })}
-        </ul>
-    )
-}
-
-DisplayUsers.propTypes = {
-    users: PropTypes.array.isRequired
-}
 
 function Search() {
     const [username, setUsername] = React.useState('')
@@ -88,7 +15,8 @@ function Search() {
         }
     )
 
-    const handleChange = (event) => setUsername(event.target.value)
+    // Handle change in input value
+    const handleInputChange = (event) => setUsername(event.target.value)
 
     const handleSearchClick = (e) => {
         e.preventDefault()
@@ -121,7 +49,7 @@ function Search() {
         }
     }
 
-    const handleKeyDown = (e) => {
+    const handleInputKeyDown = (e) => {
         if (e.key === 'Enter' && username.length !== 0) {
             handleSearchClick(e)
         }
@@ -137,8 +65,8 @@ function Search() {
                     placeholder='Search GitHub'
                     autoComplete='off'
                     value={username}
-                    onChange={handleChange}
-                    onKeyDown={handleKeyDown}
+                    onChange={handleInputChange}
+                    onKeyDown={handleInputKeyDown}
                 />
                 <button
                     className='btn'
@@ -160,7 +88,7 @@ function Search() {
                 </div>
             }
             {state.users &&
-                <DisplayUsers users={state.users} />
+                <Posts posts={state.users} />
             }
         </React.Fragment>
     );
