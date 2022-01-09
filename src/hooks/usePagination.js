6af00d8a0a3258dfa3d7aef289df4807e4ab8b7a
Stephen Carroll <stephenalexandercarroll@gmail.com>
2022-01-09 15:14:17 -0500

    Add working usePagination hook

---
+++ src/hooks/usePagination.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+export default function usePagination ({ totalCount, pageSize, siblingCount = 1, currentPage }) {
+    function range(start, end) {
+        let output = []
+        for (let i = start; i <= end; i++) {
+            output.push(i)
+        }
+        return output
+    }
+    
+    const paginationRange = React.useMemo(() => {
+        const totalPageCount = Math.ceil(totalCount / pageSize)
+        const totalPageNumbers = siblingCount + 5
+
+        // Show all pages
+        if(totalPageNumbers >= totalPageCount) {
+            return range(1, totalPageCount)
+        }
+
+        const leftSiblingIndex = Math.max(currentPage - siblingCount, 1)
+        const rightSiblingIndex = Math.min(currentPage + siblingCount, totalPageCount)
+
+        const showLeftDots = leftSiblingIndex > 2
+        const showRightDots = rightSiblingIndex < totalPageCount - 2
+
+        const firstPageIndex = 1
+        const lastPageIndex = totalPageCount
+
+        // Show only right dots
+        if (!showLeftDots && showRightDots) {
+            let leftItemCount = 3 + 2 * siblingCount
+            let leftRange = range(1, leftItemCount)
+
+            return [...leftRange, '...', totalPageCount]
+        }
+
+        // Show only left dots
+        if (showLeftDots && !showRightDots) {
+            let rightItemCount = 3 + 2 * siblingCount
+            let rightRange = range(totalPageCount - rightItemCount + 1, totalPageCount)
+
+            return [firstPageIndex, '...', ...rightRange]
+        }
+
+        // Show both left and right dots
+        if (showLeftDots && showRightDots) {
+            let middleRange = range(leftSiblingIndex, rightSiblingIndex)
+            return [firstPageIndex, '...', ...middleRange, '...', lastPageIndex]
+        }
+    }, [totalCount, pageSize, siblingCount, currentPage])
+
+    console.log(paginationRange)
+
+    return paginationRange
+}
