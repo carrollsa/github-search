7646bf04d3d049496227d005785f81b4aadeb6b5
Stephen Carroll <stephenalexandercarroll@gmail.com>
2022-01-10 17:21:15 -0500

    Modify .gitignore

---
+++ src/utils/api.js
@@ -0,0 +1,66 @@
+import {properties} from '../properties'
+
+const GQL_ACCESS_TOKEN = properties.gqlToken
+
+/*  Currently only pulls first 100 results, but I included page info to allow for cycling through paginated
+    results in the future by modifying query to accept "after: [endcursor]" as a param to search */
+export function fetchMultipleUsersGQL(username) {
+    const query = `
+        query fetchUsers($name: String!) {
+            search(query: $name, type: USER, first:100) {
+                userCount
+                pageInfo {
+                    startCursor,
+                    endCursor,
+                    hasNextPage
+                }
+                            edges {
+                    node {
+                        ...on User {
+                            login
+                            name
+                            company
+                            avatarUrl
+                            url
+                            location
+                            bio
+                            starredRepositories {
+                                totalCount
+                            }
+                            followers {
+                                totalCount
+                            }
+                            following {
+                                totalCount
+                            }
+                            twitterUsername
+                        }
+                    }
+                    textMatches {
+                        fragment
+                        property
+                        highlights {
+                            text
+                        }
+                    }
+                }
+            }
+        }`
+    return (
+        queryFetch(query, {name: username})
+    )
+}
+
+function queryFetch(query, variables) {
+    return fetch('https://api.github.com/graphql', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Token ${GQL_ACCESS_TOKEN}`
+        },
+        body: JSON.stringify({
+            query: query,
+            variables: variables
+        })
+    }).then((res) => res.json())
+}
